20c27de10b948bf19e09e5789b5992ce7e6aad24
Mourad Ait Oumaach <mr.aitoumaachmourad@gmail.com>
2026-07-23 12:56:15 +0100

    create new controller called DepartementsController to retrieve data from the departments table  and display them while the employee is creating the ticket

---
+++ HotelIT.API/Controllers/DepartmentsController.cs
@@ -0,0 +1,38 @@
+﻿using HotelIT.API.Data;
+using HotelIT.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelIT.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly HotelITDbContext _context;
+
+        public DepartmentsController(HotelITDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Departments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Departments>>> GetDepartments()
+        {
+            return await _context.Departments.ToListAsync();
+        }
+
+        // GET: api/Departments/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Departments>> GetDepartment(int id)
+        {
+            var department = await _context.Departments.FindAsync(id);
+
+            if (department == null)
+                return NotFound();
+
+            return department;
+        }
+    }
+}
